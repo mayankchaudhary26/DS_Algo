9024cdecc41e71ee7f7e423a1fe77c6abaf7cda0
Mayank Choudhary <56837137+mayankchaudhary26@users.noreply.github.com>
2021-06-13 00:52:13 +0530

    Add files via upload

---
+++ basics/cout_cin.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout<< "enter the value of num1\n";
+    cin>>num1; 
+    // this is value of num1
+    cout<< "enter the value of num2\n";
+    cin>>num2; 
+
+    cout<< "final value is :\n"<< num1+num2;
+    
+    return 0;
+}
