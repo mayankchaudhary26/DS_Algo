c3faeeaa42a26c15bcb729791715e0575d188f39
Mayank Choudhary <56837137+mayankchaudhary26@users.noreply.github.com>
2022-05-01 16:19:58 +0530

    Create Insertion sort.cpp

---
+++ Questions/Insertion sort.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<climits>
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	cout<<"enter the value"<<endl;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	for(int i=1; i<n; i++){
+		int curr = arr[i];
+		int j = i-1;
+		while(arr[j]>curr && j>=0){
+			arr[j+1]=arr[j];
+			j--;
+		} arr[j+1] = curr;
+	}
+
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<" ";
+	} cout<<endl;
+	}
+
